09c99ca0587a576884761302e084c047a52a7ba4
Chen <chen.jiang.1206@gmail.com>
2023-04-19 09:27:22 -0400

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Table from "./Table";
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const addSomthing = 'add something'
   return (
     <div className="app">
       <SushiContainer />
