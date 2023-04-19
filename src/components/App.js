87811c47a77ac7a3daaeb8af9d3a4af7e3d45769
Chen <chen.jiang.1206@gmail.com>
2023-04-19 09:45:43 -0400

    display 4 sushis on the plates

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import Reac, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([])
+  useEffect(()=>{
+    fetch(API)
+    .then(r => r.json())
+    .then(data => setSushis(data))
+  },[])
+
   const addSomthing = 'add something'
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushis={sushis}/>
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from './Sushi'
 
-function SushiContainer(props) {
+function SushiContainer({sushis}) {
+  const [sushiIndex, setSushiIndex] = useState(0)
+  //slice is used here to control display 4 pieces of sushi & non-destructible method
+
+  const renderSushis = sushis
+  .slice(sushiIndex, sushiIndex + 4)
+  .map(sushi => <Sushi key={sushi.id} sushi={sushi}/>)
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
+      {renderSushis}
       <MoreButton />
     </div>
   );
