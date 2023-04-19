17f82e16afc7c459783b08f349fc22a88d300547
Chen <chen.jiang.1206@gmail.com>
2023-04-19 10:07:44 -0400

    prevent wallet going negative

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ const API = "http://localhost:3001/sushis";
 
 function App() {
   const [sushis, setSushis] = useState([])
-  const [money, setMoney] = useState(200)
+  const [money, setMoney] = useState(50)
   useEffect(()=>{
     fetch(API)
     .then(r => r.json())
@@ -18,14 +18,17 @@ function App() {
   },[])
 
   function eatSushi(eatenSushi) {
-    const updatedSushis = sushis.map(sushi => {
-      if (sushi.id === eatenSushi.id) {
-        return {...sushi, eaten: true}
-      } return sushi
-    })
-    setSushis(updatedSushis)
-
-    setMoney(money => money - eatenSushi.price)
+    if (money >= eatenSushi.price) {
+      const updatedSushis = sushis.map(sushi => {
+        if (sushi.id === eatenSushi.id) {
+          return { ...sushi, eaten: true }
+        } return sushi
+      })
+      setSushis(updatedSushis)
+      setMoney(money => money - eatenSushi.price)
+    } else {
+      alert('Need more money')
+    }
   }
 
   const eatenSushis = sushis.filter(sushi => sushi.eaten)
